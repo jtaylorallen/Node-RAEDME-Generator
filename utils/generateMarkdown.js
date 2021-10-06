fd9f8ca3a367b25deb64219fefe4f5458112a638
J. Taylor Allen <j.taylorallen@yahoo.com>
2021-10-06 15:12:41 -0500

    markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -52,13 +52,71 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+function renderLicenseSection(license) {
+  return  `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown(data) {
+  if (data.install_code) {
+    data.install_code = `\`\`\`${data.install_code}\`\`\``
+  } else {
+    data.install_code = ``
+  }
+
+  if (data.usage_code) {
+    data.usage_code = `\`\`\`${data.usage_code}\`\`\``
+  } else {
+    data.usage_code = ``
+  }
+
+  return `# ${data.title}
+  
+  ${renderLicenseSection(data.license)}
+  
+  deployment:${data.deployedUrl}  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Content
+  
+  [Installation Instructions](#installation-instructions)  
+  [Usage Information](#usage-information)  
+  [Contribution Guidelines](#contribution-guidelines)  
+  [Test Instructions](#test-instructions)  
+  [Questions](#questions)  
+  
+  ## Installation-Instructions
+  
+  ${data.install}  
+  ${data.install_code}
+  
+  ## Usage-Information
+  
+  ${data.usage}  
+  ${data.usage_code}
+  
+  ## License
+  
+  ${data.license}
+  
+  ## Contribution-Guidelines
+  
+  ${data.contribution}
+  
+  ## Test-Instructions
+  
+  ${data.test}
+  
+  ## Questions
+  
+  GitHub Username: [@${data.github}](https://github.com/${data.github})  
+  
+  Reach out to me at [${data.email}](mailto:${data.email}) for questions  
+  `;
 }
 
 module.exports = generateMarkdown;
